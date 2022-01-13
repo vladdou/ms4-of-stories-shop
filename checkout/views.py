7f191492dffce45c6e36e0678bc6bcef64397847
flisanp <ida@ofstories.se>
2022-01-13 23:37:21 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51K7g8iLhcMTNqNlVkjUoaTFQsl1ITD2zkbw3SJ71LNQV4zdExeOSzcY5s2qhPHyeZ4INEf4sTU3dS4tHG8t49PCp00jdh5i4Wb',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

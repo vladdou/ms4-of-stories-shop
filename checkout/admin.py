e23a4abae5540e45b242de8f206aa0fa4d1997bb
flisanp <ida@ofstories.se>
2022-01-17 23:41:17 +0000

    added order history and updated checkout views

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
-# Register your models here.
-
 
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
@@ -14,14 +12,13 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total','original_bag',
-              'stripe_pid')
+                       'grand_total', 'original_bag', 'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total','original_bag',
+              'order_total', 'grand_total', 'original_bag',
               'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
@@ -30,4 +27,6 @@ class OrderAdmin(admin.ModelAdmin):
 
     ordering = ('-date',)
 
+
 admin.site.register(Order, OrderAdmin)
+

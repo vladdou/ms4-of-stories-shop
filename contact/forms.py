1b9ffa7add5a17e5981bbb9e1128b854d47086d2
flisanp <ida@ofstories.se>
2022-02-07 23:04:21 +0000

    changed placeholder text on contact form

--- contact/forms.py
+++ contact/forms.py
@@ -13,9 +13,9 @@ class ContactForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         # Setting placeholders on fields
         placeholders = {
-            'email_address': 'Email',
-            'name':  'Name',
-            'message': 'Message'
+            'email_address': 'Your Email',
+            'name':  'Your Name',
+            'message': 'Your Message'
         }
 
         self.fields['email_address'].widget.attrs['autofocus'] = True
